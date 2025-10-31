a7ce9bf19b82fd961d0feba78e89f59cd0ec503b
jarediaz <jtdiaz@umich.edu>
2025-10-31 17:07:56 -0400

    DB work for user / login stuff

--- tests/db/unit.py
+++ tests/db/unit.py
@@ -1,9 +1,12 @@
 # TO RUN TESTS: PYTHONPATH=src poetry run python -m pytest tests/db/unit.py -q
 import pytest
 from unittest.mock import MagicMock, patch
-from db.schema import Garment
+from db.schema import Garment, User
+from db.user_store import MakeUserStore, UserStoreError
 from models.enums import Category, Material
 from db.garment_store import MakeGarmentStore, GarmentStoreError
+from sqlalchemy.exc import SQLAlchemyError
+
 
 
 @pytest.fixture
@@ -16,10 +19,17 @@ def sample_garment():
         material=Material.COTTON,
         image_url="/img/test.png",
     )
+    
+@pytest.fixture
+def sample_user():
+    return User(
+        username="alice",
+        hashed_password="dummyhash",
+    )
 
 
 # verify we flush and add garment
-def test_create_success(sample_garment):
+def test_create_garment_success(sample_garment):
     session = MagicMock()
     store = MakeGarmentStore(session)
     out = store.create(sample_garment)
@@ -29,11 +39,29 @@ def test_create_success(sample_garment):
 
 
 # verify we catch DB failures
-def test_create_db_failure(sample_garment):
-    from sqlalchemy.exc import SQLAlchemyError
-
+def test_create_garment_db_failure(sample_garment):
+    
     session = MagicMock()
     session.flush.side_effect = SQLAlchemyError("boom")
     store = MakeGarmentStore(session)
     with pytest.raises(GarmentStoreError):
         store.create(sample_garment)
+        
+def test_create_user_success(sample_user):
+    session = MagicMock()
+    store = MakeUserStore(session)
+    out = store.create(sample_user)
+
+    session.add.assert_called_once_with(sample_user)
+    session.flush.assert_called_once()
+    assert out is sample_user
+
+def test_create_user_db_failure(sample_user):
+    session = MagicMock()
+    session.flush.side_effect = SQLAlchemyError("boom")
+    store = MakeUserStore(session)
+
+    with pytest.raises(UserStoreError):
+        store.create(sample_user)
+
+    session.rollback.assert_called_once()
